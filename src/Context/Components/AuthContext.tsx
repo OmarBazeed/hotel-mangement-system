24919dd6b29f2b08fc1bf62163d5c07b96c57e2e
doaasalama23 <doaasalamaadam@gmail.com>
2024-05-28 16:45:04 +0300

    login

---
+++ src/Context/Components/AuthContext.tsx
@@ -0,0 +1,64 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { jwtDecode } from "jwt-decode";
+import { ReactNode, createContext, useEffect, useState } from "react";
+// import { useTranslation } from "react-i18next";
+
+// Define the authentication data//
+export interface IAuth {
+  loginData: DecodedTokenType | null | any;
+  savLoginData: () => void;
+  userRole: string | null;
+  requestHeaders: { Authorization:string},
+  baseUrl: string | undefined;
+  // updateUserData: () => void;
+  // setUserRole: () => void;
+}
+
+interface DecodedTokenType {
+  role: string;
+  
+}
+
+export const AuthContext = createContext<IAuth | null>(null);
+
+export const AuthContextProvider: React.FC<{ children: ReactNode }> = ({children}) => {
+  const [loginData, setloginData] = useState<DecodedTokenType | null>(null);
+  const [userRole, setUserRole] = useState<string | null>(null);
+  const baseUrl="https://upskilling-egypt.com:3000/api";
+  
+  // console.log(userRole);
+  
+  const requestHeaders =
+   {
+    Authorization: ` ${localStorage.getItem("token")}`,
+    
+  };
+
+  const savLoginData = () => {
+    const encodedToken = localStorage.getItem("token");
+    const decodedToken = jwtDecode(encodedToken!) as DecodedTokenType;
+    setloginData(decodedToken);
+    setUserRole(decodedToken.role);
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      savLoginData();
+    }
+  }, []);
+
+
+
+
+
+  const contextValue: IAuth | null = {
+    loginData,
+    userRole,
+    savLoginData,
+    baseUrl,
+    requestHeaders,
+  };
+  return (
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  );
+};
