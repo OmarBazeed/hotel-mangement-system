ab88c390bbff1a58ef201bc6892d76d8749deab3
Omar Bazeed <114960595+OmarBazeed@users.noreply.github.com>
2024-06-03 17:59:01 +0300

    Building The Structure Of The Home

--- src/Modules/LandingPage/components/Home/Home.tsx
+++ src/Modules/LandingPage/components/Home/Home.tsx
@@ -1,17 +1,87 @@
-import { Box } from "@mui/material";
-
+import { Box, Container, Stack, Typography } from "@mui/material";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateRangePicker } from "@mui/x-date-pickers-pro/DateRangePicker";
+import { SingleInputDateRangeField } from "@mui/x-date-pickers-pro/SingleInputDateRangeField";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import CalendarMonthTwoToneIcon from "@mui/icons-material/CalendarMonthTwoTone";
 export default function Home() {
   return (
     <>
       <Box
-        component="p"
+        component="div"
         sx={{
           p: 2,
-          border: "1px dashed grey",
           height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        Home Section
+        <Container
+          sx={{
+            bgcolor: "teal",
+            height: "90vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "between",
+          }}
+        >
+          <Stack direction="column" useFlexGap spacing={4}>
+            <Typography
+              sx={{
+                fontWeight: "bold",
+                color: "background: rgba(21, 44, 91, 1)",
+                fontSize: "2rem",
+              }}
+            >
+              Forget Busy Work,
+              <br />
+              Start Next Vacation
+            </Typography>
+            <Typography>
+              We provide what you need to enjoy your holiday with family. Time
+              to make another memorable moments.
+            </Typography>
+            <Box>
+              <Typography>Start Booking</Typography>
+              <Box
+                sx={{
+                  position: "relative",
+                }}
+              >
+                <CalendarMonthTwoToneIcon
+                  sx={{ position: "absolute", top: "15px", left: "10px" }}
+                />
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DemoContainer components={["SingleInputDateRangeField"]}>
+                    <DateRangePicker
+                      slots={{ field: SingleInputDateRangeField }}
+                      name="allowedRange"
+                    />
+                  </DemoContainer>
+                </LocalizationProvider>
+              </Box>
+            </Box>
+          </Stack>
+          <Stack>
+            <Typography
+              sx={{
+                fontWeight: "bold",
+                color: "background: rgba(21, 44, 91, 1)",
+                fontSize: "2rem",
+              }}
+            >
+              Forget Busy Work,
+              <br />
+              Start Next Vacation
+            </Typography>
+            <Typography>
+              We provide what you need to enjoy your holiday with family. Time
+              to make another memorable moments.
+            </Typography>
+          </Stack>
+        </Container>
       </Box>
     </>
   );
