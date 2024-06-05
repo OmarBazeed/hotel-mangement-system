0d6cbb12b91f63dd7c52c30decd63c39d1c2aa03
Omar Bazeed <114960595+OmarBazeed@users.noreply.github.com>
2024-06-05 13:21:55 +0300

    Building The Branch And Make Some Progress

--- src/Modules/AdminDashboard/Componenets/Dashboard/Dashboard.tsx
+++ src/Modules/AdminDashboard/Componenets/Dashboard/Dashboard.tsx
@@ -1,3 +1,70 @@
+import { RoomServiceOutlined } from "@mui/icons-material";
+import { Box, Stack, Typography } from "@mui/material";
+
 export default function Dashboard() {
-  return <div>Charts</div>;
+  return (
+    <>
+      <Stack
+        spacing={3}
+        alignItems="center"
+        justifyContent="space-around"
+        direction="row"
+        sx={{
+          minHeight: "40vh",
+        }}
+      >
+        <Stack
+          spacing={3}
+          alignItems="center"
+          justifyContent="space-around"
+          direction="row"
+          bgcolor="gray"
+          xs={4}
+        >
+          <Box>
+            <Typography>100</Typography>
+            <Typography>Rooms</Typography>
+          </Box>
+          <Box>
+            <RoomServiceOutlined />
+          </Box>
+        </Stack>
+        <Stack
+          spacing={3}
+          alignItems="center"
+          justifyContent="space-around"
+          direction="row"
+          bgcolor="gray"
+        >
+          <Box>
+            <Typography>100</Typography>
+            <Typography>Rooms</Typography>
+          </Box>
+          <Box>
+            <RoomServiceOutlined />
+          </Box>
+        </Stack>
+        <Stack
+          spacing={3}
+          alignItems="center"
+          justifyContent="space-around"
+          direction="row"
+          bgcolor="gray"
+        >
+          <Box>
+            <Typography>100</Typography>
+            <Typography>Rooms</Typography>
+          </Box>
+          <Box>
+            <RoomServiceOutlined />
+          </Box>
+        </Stack>
+      </Stack>
+      <Stack
+        direction="row"
+        alignItems="center"
+        justifyContent="space-between"
+      ></Stack>
+    </>
+  );
 }
--- src/Modules/SharedModule/components/LayOuts/LandingPageLayout/LandingLayout.tsx
+++ src/Modules/SharedModule/components/LayOuts/LandingPageLayout/LandingLayout.tsx
@@ -2,12 +2,15 @@ import { Outlet } from "react-router-dom";
 import { DashlayoutProps } from "../../../../../Interfaces/interFaces";
 import Footer from "../../Footer/Footer";
 import Navbar from "../../Navbar/Navbar";
+import { Box } from "@mui/material";
 
 export default function LandingLayout({ setTheme }: DashlayoutProps) {
   return (
     <>
       <Navbar setTheme={setTheme} />
-      <Outlet />
+      <Box sx={{ marginTop: "65px" }}>
+        <Outlet />
+      </Box>
       <Footer />
     </>
   );
